325919683196fcac5200141e59a9307a37254804
ponpon2 <gkqr84@icloud.com>
2019-12-22 18:57:56 +0900

    initial commit

---
+++ config/application.rb
@@ -0,0 +1,9 @@
+module ChatSpace
+  class Application < Rails::Application
+    config.generators do |g|
+      g.stylesheets false
+      g.javascripts false
+      g.helper false
+      g.test_framework false
+  end
+end
